c4b5880254a0951f2b00d2237f4605bb4b149c95
dublicator@gmail.com <dublicator@gmail.com@c407cc78-f976-2888-4000-aade9f174fd3>
2011-03-22 11:11:50 +0000

    Added readFile() to Util.cs

--- trunk/Test/ImageUtilsTest.cs
+++ trunk/Test/ImageUtilsTest.cs
@@ -1,25 +1,29 @@
+using System.IO;
 using NUnit.Framework;
+using Test.Properties;
 using Word.Utils;
+using BufferedImage = System.Drawing.Image;
 
 namespace Test
 {
-    public class ImageUtilsTest:Assert
+    public class ImageUtilsTest : Assert
     {
-        	[Test]
-	public void sanityTestLocal(){
-		/*ImageUtils imageUtils = new ImageUtils();
-		Assert.NotNull(imageUtils);
-		
-		BufferedImage bufferedImage = ImageIO.read(new File(Utils.getAppRoot() + "/src/test/resources/dtpick.gif"));
-		string hexa = ImageUtils.getImageHexaBase64(bufferedImage, "gif");
-		Assert.AreEqual(1, TestUtils.regexCount(hexa, "R0lGODlhEAAQAPMAAKVNSkpNpUpNSqWmpdbT1v"));*/
-	}
-	
-	[Test]
-	public void exceptionTest()
-    {
-		/*URL url = new URL("http://localhost:8080/ExampleStruts/img/bullshit.gif");
-		BufferedImage bufferedImage = ImageIO.read(url);*/
-	}
+        [Test]
+        public void sanityTestLocal()
+        {
+            //ImageUtils imageUtils = new ImageUtils();
+            //Assert.NotNull(imageUtils);
+            BufferedImage bufferedImage = Resources.dtpick;
+            string hexa = ImageUtils.getImageHexaBase64(bufferedImage, "gif");
+            //Assert.AreEqual(1, TestUtils.regexCount(hexa, "R0lGODlhEAAQAPMAAKVNSkpNpUpNSqWmpdbT1v"));
+            Assert.AreEqual(1, TestUtils.regexCount(hexa, "R0lGODlhEAAQAKIAAKVNSkpNpUpNSqWmpdbT1v"));
+        }
+
+        [Test]
+        public void exceptionTest()
+        {
+            /*URL url = new URL("http://localhost:8080/ExampleStruts/img/bullshit.gif");
+            BufferedImage bufferedImage = ImageIO.read(url);*/
+        }
     }
 }
