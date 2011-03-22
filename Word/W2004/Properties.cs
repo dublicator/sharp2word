15573b5af691280c1973667df0963effa2333d17
Anton Kandybo <dublicator@hotmail.com>
2011-03-22 08:01:52 +0000

    Added comments to all properties of Properties.cs

--- Word/W2004/Properties.cs
+++ Word/W2004/Properties.cs
@@ -4,35 +4,8 @@ using Word.Api.Interfaces;
 
 namespace Word.W2004
 {
-    public class Properties:IElement
+    public class Properties : IElement
     {
-        public string Title{get; set; }
-        public string Subject { get; set; }
-        public string Keywords { get; set; }
-        public string Description { get; set; }
-        public string Category { get; set; }
-        public string Author { get; set; }
-        public string LastAuthor { get; set; }
-        public string Manager { get; set; }
-        public string Company { get; set; }
-        public string HyperlinkBase { get; set; }
-        public int Revision { get; set; }
-        public string PresentationFormat { get; set; }
-        public string Guid { get; set; }
-        //public string AppName { get; set; }
-        public string Version { get; set; }
-        public int TotalTime { get; set; }
-        public DateTime LastPrinted { get; set; }
-        public DateTime Created { get; set; }
-        public DateTime LastSaved { get; set; }
-        public int Pages { get; set; }
-        public int Words { get; set; }
-        public int Characters { get; set; }
-        public int CharactersWithSpaces { get; set; }
-        public int Bytes { get; set; }
-        public int Lines { get; set; }
-        public int Paragraphs { get; set; }
-
         private const string _props =
             "    <o:DocumentProperties> \n"
             + "        <o:Title>{Title}</o:Title> \n"
@@ -63,7 +36,7 @@ namespace Word.W2004
             + "        <o:Paragraphs>{Paragraphs}</o:Paragraphs> \n"
             + "    </o:DocumentProperties> ";
 
-        readonly StringBuilder content = new StringBuilder("");
+        private readonly StringBuilder content = new StringBuilder("");
 
         #region Implementation of IElement
 
@@ -82,7 +55,7 @@ namespace Word.W2004
         /// <value>This is the string value of the element ready to be appended/inserted in the Document.</value>
         public string Content
         {
-            get 
+            get
             {
                 if ("".Equals(this.content.ToString()))
                 {
@@ -119,6 +92,141 @@ namespace Word.W2004
         }
 
         #endregion
+
+        /// <summary>
+        ///   Represents the title of the document. The title can be different than the file name. 
+        ///   The title is used when searching for the document and also when creating Web pages from the document.
+        /// </summary>
+        public string Title { get; set; }
+
+        /// <summary>
+        ///   Represents the subject of the document. This property can be used to group similar files together, 
+        ///   so you can search for all files that have the same subject.
+        /// </summary>
+        public string Subject { get; set; }
+
+        /// <summary>
+        ///   Represents keywords to be used when searching for the document.
+        /// </summary>
+        public string Keywords { get; set; }
+
+        /// <summary>
+        ///   Represents comments to be used when searching for the document.
+        /// </summary>
+        public string Description { get; set; }
+
+        /// <summary>
+        ///   Represents the category of the document. 
+        ///   This property can be used to group similar files together, so you can search for all files that have the same category.
+        /// </summary>
+        public string Category { get; set; }
+
+        /// <summary>
+        ///   Represents the author who created the document.
+        /// </summary>
+        public string Author { get; set; }
+
+        /// <summary>
+        ///   Represents the name of the author who last saved the document.
+        /// </summary>
+        public string LastAuthor { get; set; }
+
+        /// <summary>
+        ///   Represents the manager of the author of the document. 
+        ///   This property can be used to group similar files together, so you can search for all the files that have the same manager.
+        /// </summary>
+        public string Manager { get; set; }
+
+        /// <summary>
+        ///   Represents the company that employs the author. This property can be used to group similar files together, 
+        ///   so you can search for all files that have the same company.
+        /// </summary>
+        public string Company { get; set; }
+
+        /// <summary>
+        ///   Represents the path or URL that is used for all hyperlinks with the same 
+        ///   base address that are inserted in the document. 
+        ///   This can be an Internet address (for example, http://www.microsoft.com), 
+        ///   a path to a folder on your hard disk (for example, c:\personal\documents), 
+        ///   or a path to a folder on a network (for example, \\myserver\public\documents).
+        /// </summary>
+        public string HyperlinkBase { get; set; }
+
+        /// <summary>
+        ///   Represents the number of times the document has been saved.
+        /// </summary>
+        public int Revision { get; set; }
+
+        /// <summary>
+        ///   Represents the presentation format of the document.
+        /// </summary>
+        public string PresentationFormat { get; set; }
+
+        /// <summary>
+        ///   Represents the globally unique identifier for the document.
+        /// </summary>
+        public string Guid { get; set; }
+
+        //public string AppName { get; set; }
+        /// <summary>
+        ///   Represents the version number of the application that created the document.
+        /// </summary>
+        public string Version { get; set; }
+
+        /// <summary>
+        ///   Represents the number of minutes that the document has been open for editing since it was created.
+        /// </summary>
+        public int TotalTime { get; set; }
+
+        /// <summary>
+        ///   Represents the date and time that the document was last printed.
+        /// </summary>
+        public DateTime LastPrinted { get; set; }
+
+        /// <summary>
+        ///   Represents the date and time that the document was originally created.
+        /// </summary>
+        public DateTime Created { get; set; }
+
+        /// <summary>
+        ///   Represents the date and time that the document was last saved.
+        /// </summary>
+        public DateTime LastSaved { get; set; }
+
+        /// <summary>
+        ///   Represents an estimate of the number of pages in the document.
+        /// </summary>
+        public int Pages { get; set; }
+
+        /// <summary>
+        ///   Represents an estimate of the number of words in the document.
+        /// </summary>
+        public int Words { get; set; }
+
+        /// <summary>
+        ///   Represents an estimate of the number of characters in the document.
+        /// </summary>
+        public int Characters { get; set; }
+
+        /// <summary>
+        ///   Represents an estimate of the number of characters (including spaces) in the document.
+        /// </summary>
+        public int CharactersWithSpaces { get; set; }
+
+        /// <summary>
+        ///   Represents an estimate of the number of bytes in the document.
+        /// </summary>
+        public int Bytes { get; set; }
+
+        /// <summary>
+        ///   Represents an estimate of the number of lines in the document.
+        /// </summary>
+        public int Lines { get; set; }
+
+        /// <summary>
+        ///   Represents an estimate of the number of paragraphs in the document.
+        /// </summary>
+        public int Paragraphs { get; set; }
 
         private static string FormatDate(DateTime time)
         {
